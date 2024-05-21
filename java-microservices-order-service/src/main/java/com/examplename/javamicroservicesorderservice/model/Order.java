68a9b34ad3bf29f8fad467c91d8c58ea7f8104d8
larinpaul <www.larinpavel@mail.ru>
2024-05-21 07:28:54 +0500

    Created the Order class, but import javax.persistence. doesn't seem to work...

--- java-microservices-order-service/src/main/java/com/examplename/javamicroservicesorderservice/model/Order.java
+++ java-microservices-order-service/src/main/java/com/examplename/javamicroservicesorderservice/model/Order.java
@@ -1,11 +1,25 @@
 package com.examplename.javamicroservicesorderservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
-
-
-
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String orderNumber;
+    private List<OrderLineItems> orderLineItemsList;
 
 }
