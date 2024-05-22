32e1190ab74a436c4aaec962562257516801d5aa
larinpaul <www.larinpavel@mail.ru>
2024-05-22 13:11:06 +0500

    Created OrderLineItems. @Entity @Table(name = "t_order_line_items") @Getter @Setter @AllArgsConstructor @NoArgsConstructor public class OrderLineItems { @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id; private String skuCode; private BigDecimal price; private Integer quantity; }

---
+++ java-microservices-order-service/src/main/java/com/examplename/javamicroservicesorderservice/model/OrderLineItems.java
@@ -0,0 +1,27 @@
+package com.examplename.javamicroservicesorderservice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_order_line_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+
+}
