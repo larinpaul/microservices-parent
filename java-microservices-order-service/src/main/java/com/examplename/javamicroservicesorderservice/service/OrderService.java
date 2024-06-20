798421d5709c47cbaa3112e8c9f18d364dd09115
larinpaul <www.larinpavel@mail.ru>
2024-06-20 07:29:42 +0500

    Changed OrderService.java... package com.examplename.javamicroserviceorderservice.service; import com.examplename.javamicroserviceorderservice.dto.OrderLineItemsDto; import com.examplename.javamicroservicesorderservice.dto.OrderRequest; import com.examplename.javamicroserviceorderservice.model.Order; import com.examplename.javamicroserviceorsedservice.repository.OrderRepository; import lombok.RequiredArgstConstructor; import org.springframework.stereotype.Service; import java.util.List; import java.util.UUID;  @Service @RequiredArgsConstructor public class OrderService { private final OrderRepository orderRepository; public void placeOrder(OrderRequest orderRequest) { Order order = new Order(); order.setOrderNumber(UUID.randomUUID().toString()); List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList().stream().map(this::mapToDto).toList();  order.setOrderLineItemsList(orderLineItems); orderRepository.save(order);  private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) { OrderLineItems orderLineItems = new OrderLineItems(); orderLIneItems.setPrice(orderLineItemsDto.getPrice()); orderLineItems.setQuantity(orderLineItemsDto.getQuantity()); orderLineItems.setSkuCode(orderLineItems.getSkuCode()); return orderLineItems; } }

--- java-microservices-order-service/src/main/java/com/examplename/javamicroservicesorderservice/service/OrderService.java
+++ java-microservices-order-service/src/main/java/com/examplename/javamicroservicesorderservice/service/OrderService.java
@@ -4,13 +4,18 @@ import com.examplename.javamicroservicesorderservice.dto.OrderLineItemsDto;
 import com.examplename.javamicroservicesorderservice.dto.OrderRequest;
 import com.examplename.javamicroservicesorderservice.model.Order;
 import com.examplename.javamicroservicesorderservice.model.OrderLineItems;
+import com.examplename.javamicroservicesorderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository orderRepository;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -22,6 +27,8 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
